93cd9a0e460dc8ee3777cbdac2e847d9bd5b986e
Rei Rigos <mcrei1018@gmail.com>
2024-09-25 13:16:29 +0800

    Pseudocode for the Program

--- exercise_2_ohm's law_calculator.py
+++ exercise_2_ohm's law_calculator.py
@@ -1,3 +1,4 @@
+# Creating Functions for each Formula
 def voltage_formula(current, resistance):
     return current * resistance
 
@@ -11,35 +12,38 @@ def resistance_formula(voltage, current):
         raise ZeroDivisionError("Sorry, Current cannot be Zero.")
     return voltage / current
 
+# Creating the Calculator
 while True:
+     # Creating The User Prompt
     try:
         print("What Do You Want to Calculate?")
         print("Press 'V' for Voltage, 'I' for Current, 'R' for Resistance")
         choice = str(input("Enter Your Choice ('V', 'I', or 'R'): ")).upper()
         
+        # Calculator for Voltage 
         if choice == "V":
             current = float(input("Enter the Current (in Amps): "))
             resistance = float(input("Enter the Resistance (in Ohms): "))
             voltage = voltage_formula(current, resistance)
             print(f"The Voltage is {round(voltage, 2)} V")
-
+        # Calculator for Current 
         elif choice == "I":
             voltage = float(input("Enter the Voltage (in Volts): "))
             resistance = float(input("Enter the Resistance (in Ohms): "))
             current = current_formula(voltage, resistance)
             print(f"The Current is {round(current, 2)} A") 
-
+        # Calculator for Resistance
         elif choice == "R":
             voltage = float(input("Enter the Voltage (in Volts): "))
             current = float(input("Enter the Current (in Amps): "))
             resistance = resistance_formula(voltage, current)
             print(f"The Resistance is {round(resistance, 2)} Ω")   
-
+        # Other Possible Options
         else:
             print("Please Enter Valid Options ('V', 'I', or 'R').")
             continue
         break
-
+    # Handle the Exceptions
     except ValueError:  
         print("You have put an Invalid Input, Please Enter Numeric Value for voltage, current, and resistance.")
 
