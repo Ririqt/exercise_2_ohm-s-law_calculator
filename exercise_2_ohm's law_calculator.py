1a38790f7c5b254b6ca17841625959db1f889f28
Rei Rigos <mcrei1018@gmail.com>
2024-09-25 12:17:12 +0800

    Creating Function for the Resistance Formula

--- exercise_2_ohm's law_calculator.py
+++ exercise_2_ohm's law_calculator.py
@@ -5,3 +5,6 @@ def current_formula(voltage, resistance):
     if resistance == 0:
         raise ZeroDivisionError("Sorry, Resistance cannot be Zero.")
     return voltage / resistance
+
+def resistance_formula(voltage, current):
+    
